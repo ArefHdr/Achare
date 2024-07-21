2632b607fd80808b086d8f802e1a8c3389bfbfd8
ArefHdr <arefheydarii0023@gmail.com>
2024-07-21 10:56:21 +0000

    add trim and change map.vue local

--- src/pages/AddressRegistration.vue
+++ src/pages/AddressRegistration.vue
@@ -8,15 +8,26 @@
         <form class="widget-form">
           <div class="row widget__row">
             <div class="col-12">
-              <p class="widget__form-title">لطفا مشخصات و آدرس خود را وارد کنید</p>
+              <p class="widget__form-title">
+                لطفا مشخصات و آدرس خود را وارد کنید
+              </p>
             </div>
             <div class="col-12 col-sm-6 col-md-6 col-lg-4">
               <div class="form-items">
                 <label class="form-label">نام</label>
                 <div class="form-input-box">
-                  <input @input="validateFirstName" v-model="firstNameValue" class="form-input" type="text"
-                         placeholder="مثال:عارف">
-                  <i v-show="firstNameValue" @click="clearInput('firstNameValue')" class="bi bi-x-circle-fill"></i>
+                  <input
+                    @input="validateFirstName"
+                    v-model.trim="firstNameValue"
+                    class="form-input"
+                    type="text"
+                    placeholder="مثال:عارف"
+                  />
+                  <i
+                    v-show="firstNameValue"
+                    @click="clearInput('firstNameValue')"
+                    class="bi bi-x-circle-fill"
+                  ></i>
                 </div>
                 <div class="input-error" v-if="!isFirstNameValid">
                   <span>نام باید دارای 3 کاراکتر باشد</span>
@@ -27,9 +38,18 @@
               <div class="form-items">
                 <label class="form-label">نام و نام خانوادگی</label>
                 <div class="form-input-box">
-                  <input @input="validateLastName" v-model="lastNameValue" class="form-input" type="text"
-                         placeholder="مثال:حیدری">
-                  <i v-show="lastNameValue" @click="clearInput('lastNameValue')" class="bi bi-x-circle-fill"></i>
+                  <input
+                    @input="validateLastName"
+                    v-model.trim="lastNameValue"
+                    class="form-input"
+                    type="text"
+                    placeholder="مثال:حیدری"
+                  />
+                  <i
+                    v-show="lastNameValue"
+                    @click="clearInput('lastNameValue')"
+                    class="bi bi-x-circle-fill"
+                  ></i>
                 </div>
                 <div class="input-error" v-if="!isLastNameValid">
                   <span>نام خانوادگی باید دارای 3 کاراکتر باشد</span>
@@ -40,9 +60,18 @@
               <div class="form-items">
                 <label class="form-label">شماره تلفن همراه</label>
                 <div class="form-input-box">
-                  <input @input="validateMobile" v-model="mobileValue" class="form-input" type="text"
-                         placeholder="مثال:09164330023">
-                  <i v-show="mobileValue" @click="clearInput('mobileValue')" class="bi bi-x-circle-fill"></i>
+                  <input
+                    @input="validateMobile"
+                    v-model.trim="mobileValue"
+                    class="form-input"
+                    type="text"
+                    placeholder="مثال:09164330023"
+                  />
+                  <i
+                    v-show="mobileValue"
+                    @click="clearInput('mobileValue')"
+                    class="bi bi-x-circle-fill"
+                  ></i>
                 </div>
                 <div class="input-error" v-if="!isMobileValid">
                   <span>شماره وارد شده صحیح نمیباشد</span>
@@ -56,9 +85,18 @@
                   <span class="form-label">*با پیش شماره</span>
                 </div>
                 <div class="form-input-box">
-                  <input @input="validatePhone" v-model="phoneValue" class="form-input" type="text"
-                         placeholder="مثال:06643240206">
-                  <i v-show="phoneValue" @click="clearInput('phoneValue')" class="bi bi-x-circle-fill"></i>
+                  <input
+                    @input="validatePhone"
+                    v-model.trim="phoneValue"
+                    class="form-input"
+                    type="text"
+                    placeholder="مثال:06643240206"
+                  />
+                  <i
+                    v-show="phoneValue"
+                    @click="clearInput('phoneValue')"
+                    class="bi bi-x-circle-fill"
+                  ></i>
                 </div>
                 <div class="input-error" v-if="!isPhoneValid">
                   <span>شماره وارد شده صحیح نمیباشد</span>
@@ -69,8 +107,17 @@
               <div class="form-items">
                 <label class="form-label">آدرس</label>
                 <div class="form-input-box">
-                  <input @input="validateAddress" v-model="addressValue" class="form-input" type="text">
-                  <i v-show="addressValue" @click="clearInput('addressValue')" class="bi bi-x-circle-fill"></i>
+                  <input
+                    @input="validateAddress"
+                    v-model.trim="addressValue"
+                    class="form-input"
+                    type="text"
+                  />
+                  <i
+                    v-show="addressValue"
+                    @click="clearInput('addressValue')"
+                    class="bi bi-x-circle-fill"
+                  ></i>
                 </div>
                 <div class="input-error" v-if="!isAddressValid">
                   <span>آدرس باید حداقل 3 کاراکتر باشد</span>
@@ -85,11 +132,21 @@
                   </div>
                   <div class="gender-items d-flex gap-3">
                     <div class="d-flex gap-1">
-                      <input type="radio" id="female" v-model="genderValue" value="female">
+                      <input
+                        type="radio"
+                        id="female"
+                        v-model="genderValue"
+                        value="female"
+                      />
                       <label for="female">خانم</label>
                     </div>
                     <div class="d-flex gap-1">
-                      <input type="radio" id="male" v-model="genderValue" value="male">
+                      <input
+                        type="radio"
+                        id="male"
+                        v-model="genderValue"
+                        value="male"
+                      />
                       <label for="male">آقا</label>
                     </div>
                   </div>
@@ -104,7 +161,10 @@
       <div class="row">
         <div class="col-12">
           <div class="widget__submit">
-            <base-button :disable-status="isDisable" @click="submitForm"></base-button>
+            <base-button
+              :disable-status="isDisable"
+              @click="submitForm"
+            ></base-button>
           </div>
         </div>
       </div>
@@ -113,18 +173,18 @@
 </template>
 <script setup>
 import useAddressStore from "@/store/addresses.js";
-import {computed, ref, onMounted, onBeforeUnmount} from "vue";
+import { computed, ref, onMounted, onBeforeUnmount } from "vue";
 import BaseButton from "@/components/base/BaseButton.vue";
 import router from "@/router";
-import {isAuthenticated} from '@/router/index.js'
+import { isAuthenticated } from "@/router/index.js";
 
-const addressStore = useAddressStore()
-const firstNameValue = ref('')
-const lastNameValue = ref('')
-const mobileValue = ref('')
-const phoneValue = ref('')
-const addressValue = ref('')
-const genderValue = ref('')
+const addressStore = useAddressStore();
+const firstNameValue = ref("");
+const lastNameValue = ref("");
+const mobileValue = ref("");
+const phoneValue = ref("");
+const addressValue = ref("");
+const genderValue = ref("");
 const isFirstNameValid = ref(true);
 const isLastNameValid = ref(true);
 const isMobileValid = ref(true);
@@ -140,11 +200,13 @@ const validateLastName = () => {
 };
 
 const validateMobile = () => {
-  isMobileValid.value = mobileValue.value.length === 11 && /^09\d{9}$/.test(mobileValue.value);
+  isMobileValid.value =
+    mobileValue.value.length === 11 && /^09\d{9}$/.test(mobileValue.value);
 };
 
 const validatePhone = () => {
-  isPhoneValid.value = phoneValue.value.length === 11 && /^\d+$/.test(phoneValue.value);
+  isPhoneValid.value =
+    phoneValue.value.length === 11 && /^\d+$/.test(phoneValue.value);
 };
 
 const validateAddress = () => {
@@ -165,35 +227,37 @@ const submitForm = () => {
     lng: null,
     gender: genderValue.value,
     address_id: "test",
-    id: 1
-  })
-  addressStore.addNewAddressInfo(formData)
-  router.push({path: '/address-registration/map'})
-}
+    id: 1,
+  });
+  addressStore.addNewAddressInfo(formData);
+  router.push({ path: "/address-registration/map" });
+};
 
 const isFormValid = computed(() => {
   return (
-      isFirstNameValid.value &&
-      isLastNameValid.value &&
-      isMobileValid.value &&
-      isPhoneValid.value &&
-      isAddressValid.value &&
-      !!genderValue.value
+    isFirstNameValid.value &&
+    isLastNameValid.value &&
+    isMobileValid.value &&
+    isPhoneValid.value &&
+    isAddressValid.value &&
+    !!genderValue.value
   );
 });
 const clearInput = (inputValue) => {
   eval(`${inputValue}.value = '';`);
-}
-const containerClass = ref('container');
+};
+const containerClass = ref("container");
 const updateContainerClass = () => {
-  window.innerWidth >= 768 ? containerClass.value = 'custom-container' : containerClass.value = 'container';
+  window.innerWidth >= 768
+    ? (containerClass.value = "custom-container")
+    : (containerClass.value = "container");
 };
 onMounted(() => {
   updateContainerClass();
-  window.addEventListener('resize', updateContainerClass);
+  window.addEventListener("resize", updateContainerClass);
 });
 onBeforeUnmount(() => {
-  window.removeEventListener('resize', updateContainerClass);
+  window.removeEventListener("resize", updateContainerClass);
 });
 </script>
 
@@ -210,7 +274,7 @@ onBeforeUnmount(() => {
 
 .form-input-box {
   width: 100%;
-  border: 1px solid #D4D4D4;
+  border: 1px solid #d4d4d4;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -220,7 +284,7 @@ onBeforeUnmount(() => {
 }
 
 .form-input-box:focus-within {
-  border: 1px solid #00BFA5
+  border: 1px solid #00bfa5;
 }
 
 .form-input {
@@ -235,7 +299,7 @@ onBeforeUnmount(() => {
 }
 
 .bi-x-circle-fill {
-  color: #B6B6B6;
+  color: #b6b6b6;
   cursor: pointer;
 }
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,40 @@
-import {createWebHistory, createRouter} from 'vue-router';
+import { createWebHistory, createRouter } from "vue-router";
 import AddressRegistration from "@/pages/AddressRegistration.vue";
 import AddressList from "@/pages/AddressList.vue";
-import Map from "@/components/map/Map.vue";
-import {ref} from "vue";
-export const isAuthenticated = ref(false);  //به صفحه map قبل از پر کردن اطلاعات در صفحه فرم اجازه ورود نمیده
+import Map from "@/components/sections/Map.vue";
+import { ref } from "vue";
+export const isAuthenticated = ref(false); //به صفحه map قبل از پر کردن اطلاعات در صفحه فرم اجازه ورود نمیده
 
 const routes = [
-    {name: 'home', path: '/', component: AddressRegistration}, //صفحه پیشفرض
-    {name: 'addressRegistration', path: '/address-registration', component: AddressRegistration},
-    {name: 'addressRegistrationMap', path: '/address-registration/map', component: Map, meta: {requiresAuth: true}},
-    {name: 'addressList', path: '/address-list', component: AddressList},
-]
+  { name: "home", path: "/", component: AddressRegistration }, //صفحه پیشفرض
+  {
+    name: "addressRegistration",
+    path: "/address-registration",
+    component: AddressRegistration,
+  },
+  {
+    name: "addressRegistrationMap",
+    path: "/address-registration/map",
+    component: Map,
+    meta: { requiresAuth: true },
+  },
+  { name: "addressList", path: "/address-list", component: AddressList },
+];
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes,
-    scrollBehavior() { //با تغییر صفحه اسکرول به ابتدای صفحه میرود
-        return {top: 0}
-    }
-})
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+  scrollBehavior() {
+    //با تغییر صفحه اسکرول به ابتدای صفحه میرود
+    return { top: 0 };
+  },
+});
 router.beforeEach((to, from, next) => {
-    if (to.meta.requiresAuth && !isAuthenticated.value) { //به صفحه map قبل از پر کردن اطلاعات در صفحه فرم اجازه ورود نمیده
-        next('/address-registration');
-    } else {
-        next();
-    }
-})
-export default router
+  if (to.meta.requiresAuth && !isAuthenticated.value) {
+    //به صفحه  قبل از پر کردن اطلاعات در صفحه ورود به صفحه map را ندارد
+    next("/address-registration");
+  } else {
+    next();
+  }
+});
+export default router;
